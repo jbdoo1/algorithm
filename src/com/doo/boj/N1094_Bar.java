0600e9d4c3d42b6ddbeed12b66cc1c9c6a427d4e
Byeongdoo Jeon <bdoo1.jeon@gmail.com>
2017-10-28 16:40:27 +0900

    [BOJ] 1094
    DP

---
+++ src/com/doo/boj/N1094_Bar.java
@@ -0,0 +1,45 @@
+package com.doo.boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class N1094_Bar {
+
+	static int target, result, cutCount = 1, barCount = 1;
+	final static int X = 64;
+	static int remainX = X;
+	
+	public static void main(final String[] args) {
+		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			String line = br.readLine();
+			target = Integer.parseInt(line);
+			
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			bw.write(String.valueOf(dp()));
+			bw.flush();
+			bw.close();
+		} catch (final IOException ie) {
+			ie.printStackTrace();
+		}
+	}
+	
+	static int dp() {
+		if (remainX == target) return barCount;
+		
+		boolean isCut = remainX - X / Math.pow(2, cutCount) >= target;
+		if (isCut) {
+			remainX -= X / Math.pow(2, cutCount);
+		} else {
+			barCount++;
+		}
+		cutCount++;
+		
+		return dp();
+	}
+}
