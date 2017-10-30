f72f4dd575d55c0042f88ed2be9a8bc5850f2554
Byeongdoo Jeon <bdoo1.jeon@gmail.com>
2017-10-31 00:57:00 +0900

    [BOJ] 1094
    add sample data files

--- src/com/doo/boj/N1094_Bar.java
+++ src/com/doo/boj/N1094_Bar.java
@@ -2,6 +2,7 @@ package com.doo.boj;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -15,7 +16,8 @@ public class N1094_Bar {
 	public static void main(final String[] args) {
 		
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedReader br = new BufferedReader(new FileReader("/Users/jeonbyeongdoo/Documents/workspace/algorithm/resources/N1094_SampleData_4.txt"));
 			
 			String line = br.readLine();
 			target = Integer.parseInt(line);
